54dcbab0dc8b55c20f239812a5af727af98bf45f
andresdanielmtz <autummata@gmail.com>
2025-05-16 13:46:06 -0600

    Add: Navbar Title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -66,6 +66,10 @@ ListItem.displayName = "ListItem";
 const NavBar = () => {
   return (
     <NavigationMenu style={{ position: "static" }}>
+      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        Mobook
+      </h2>
+
       <NavigationMenuList>
         {Links.map((link) => (
           <NavigationMenuItem key={link.title}>
