b5197b317cb6a1598da0f5460db402bc13b13a12
andresdanielmtz <autummata@gmail.com>
2025-05-19 12:53:58 -0600

    Add: Basename Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./views/Home";
 import NavBar from "./components/NavBar";
 import { ProfileView } from "./views/Profile";
@@ -7,7 +7,7 @@ import { SearchView } from "./views/Search";
 
 const AppRouter: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/mobook">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
