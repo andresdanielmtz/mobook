b4f99c6a1fc5f2ac60b5e972ae3468cedd98406b
andresdanielmtz <autummata@gmail.com>
2025-05-19 13:31:35 -0600

    Add: Fallback Cover

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -10,17 +10,21 @@ const BookCard = ({ imageLinks, title }: ICard) => {
     <div
       key={title}
       tabIndex={0}
-      className="w-48 rounded overflow-hidden relative group focus:outline-none cursor-pointer"
+      className="w-48 rounded overflow-hidden relative group focus:outline-none cursor-pointer my-5"
     >
-      <img
-        className="w-full h-48 object-contain"
-        src={
-          imageLinks && imageLinks.thumbnail
-            ? imageLinks.thumbnail
-            : "/fallback-image.png"
-        }
-        alt={title}
-      />
+      {!imageLinks ? (
+        <div className="w-full h-48 object-contain content-center justify-items-center bg-gray-300">
+          <p className="leading-7 [&:not(:first-child)]:mt-6 text-black">
+            {" "}
+            {title}{" "}
+          </p>
+        </div>
+      ) : (
+        <img
+          className="w-full h-48 object-contain"
+          src={imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : ""}
+        />
+      )}
       <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white p-2 text-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-focus:opacity-100">
         {title}
       </div>
