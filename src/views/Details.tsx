3882e476693b1cf083d0d614ecb0f7ef27b3e8d7
andresdanielmtz <autummata@gmail.com>
2025-05-24 18:56:40 -0600

    Update: Return Button Alignment

--- src/views/Details.tsx
+++ src/views/Details.tsx
@@ -56,62 +56,66 @@ const Details = () => {
   }
 
   return (
-    <div className="container mx-auto p-6">
-      <div className="flex flex-row items-center justify-between align-middle">
+    <div>
+      <div className="flex justify-start align-bottom mt-5 ml-5 ">
         <Button onClick={handleBack}> Back</Button>
-        <h1 className="text-3xl font-bold text-gray-800 mb-6 text-justify">
-          {data.volumeInfo.title}
-        </h1>
       </div>
-      <div className="flex flex-col md:flex-row items-start">
-        {data.volumeInfo.imageLinks?.thumbnail && (
-          <img
-            className="w-full md:w-1/3 rounded shadow-md mb-6 md:mb-0 md:mr-6"
-            src={data.volumeInfo.imageLinks?.thumbnail}
-            alt={data.volumeInfo.title}
-          />
-        )}
-        <div className="flex-1 text-justify">
-          <Markup content={data.volumeInfo.description} />
+      <div className="container mx-auto p-6">
+        <div className="flex flex-row items-center justify-between align-middle">
+          <h1 className="text-3xl font-bold text-gray-800 mb-6 text-justify">
+            {data.volumeInfo.title}
+          </h1>
+        </div>
+        <div className="flex flex-col md:flex-row items-start">
+          {data.volumeInfo.imageLinks?.thumbnail && (
+            <img
+              className="w-full md:w-1/3 rounded shadow-md mb-6 md:mb-0 md:mr-6"
+              src={data.volumeInfo.imageLinks?.thumbnail}
+              alt={data.volumeInfo.title}
+            />
+          )}
+          <div className="flex-1 text-justify">
+            <Markup content={data.volumeInfo.description} />
 
-          <h3 className="text-xl font-semibold text-gray-800 mb-4  my-5">
-            Authors
-          </h3>
-          <ul className="list-disc list-inside mb-6">
-            {data.volumeInfo.authors?.map((author, index) => (
-              <li key={index} className="text-gray-600">
-                {author}
-              </li>
-            ))}
-
-            <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
-              {" "}
-              Publisher
-            </h3>
-            <p className="text-gray-600 mb-6">{data.volumeInfo.publisher}</p>
-            {data.volumeInfo.publishedDate ? (
-              <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
-                {" "}
-                Published Date: {data.volumeInfo.publishedDate}
-              </h3>
-            ) : (
-              <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
-                {" "}
-                Publish Date Unavailable
-              </h3>
-            )}
-
-            <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
-              Categories
+            <h3 className="text-xl font-semibold text-gray-800 mb-4  my-5">
+              Authors
             </h3>
             <ul className="list-disc list-inside mb-6">
-              {data.volumeInfo.categories?.map((category, index) => (
+              {data.volumeInfo.authors?.map((author, index) => (
                 <li key={index} className="text-gray-600">
-                  {category}
+                  {author}
                 </li>
               ))}
+
+              <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
+                {" "}
+                Publisher
+              </h3>
+              <p className="text-gray-600 mb-6">{data.volumeInfo.publisher}</p>
+              {data.volumeInfo.publishedDate ? (
+                <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
+                  {" "}
+                  Published Date: {data.volumeInfo.publishedDate}
+                </h3>
+              ) : (
+                <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
+                  {" "}
+                  Publish Date Unavailable
+                </h3>
+              )}
+
+              <h3 className="text-xl font-semibold text-gray-800 mb-4 my-5">
+                Categories
+              </h3>
+              <ul className="list-disc list-inside mb-6">
+                {data.volumeInfo.categories?.map((category, index) => (
+                  <li key={index} className="text-gray-600">
+                    {category}
+                  </li>
+                ))}
+              </ul>
             </ul>
-          </ul>
+          </div>
         </div>
       </div>
     </div>
