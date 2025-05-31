ed39830a6d34def759be3cc439df982c56672cec
andresdanielmtz <autummata@gmail.com>
2025-05-30 21:28:13 -0600

    Fix: Google Forms Error Bug

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -22,9 +22,7 @@ const LoginView = () => {
       return resp.uid;
     } catch (e) {
       console.log(`Error: ${e}`);
-      toast.error(
-        "Could not log in. Please check your credentials and try again.",
-      );
+      toast.error("Login failed, please check your credentials and try again.");
     }
   }
 
@@ -35,7 +33,7 @@ const LoginView = () => {
       toast.success("Login successful");
     } catch {
       console.error("Google sign-in failed");
-      toast.error("Google sign-in failed. Please try again.");
+      toast.error("Google sign-in failed, please try again.");
     }
   }
 
@@ -43,48 +41,47 @@ const LoginView = () => {
     <div>
       <h1 className="text-4xl font-bold text-center mt-10">Login</h1>
       <div className="max-w-md mx-auto mt-10">
-        <form
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          onSubmit={handleLogin}
-        >
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="text"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="*******"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <Button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-md mt-1 mx-auto"
-              type="submit"
-            >
-              Sign In
-            </Button>
-          </div>
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form onSubmit={handleLogin}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="*******"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <Button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-md mt-1 mx-auto"
+                type="submit"
+              >
+                Sign In
+              </Button>
+            </div>
+          </form>
           <Button
             variant="outline"
             onClick={handleGoogleLogin}
@@ -101,7 +98,7 @@ const LoginView = () => {
               <span>Sign in with Google</span>
             </div>
           </Button>
-        </form>
+        </div>
       </div>
 
       <div className="flex flex-col">
